e22a3e21800c265f29c03698202e66bb2cc5d815
JoyZadan <joyzadan.learning@gmail.com>
2022-04-19 21:05:40 +0000

    Saved unsaved quesries_count_function.sql file

--- queries_count_function.sql
+++ queries_count_function.sql
@@ -15,18 +15,18 @@ where the customer's `FirstName` is "Frank"
 SELECT COUNT(*) FROM Customer WHERE FirstName = 'Frank';
 
 /*
-Challenge 1 Solution - How Many Customers is Employee 4 the Sales Support Agent For?
+Challenge 1 and Solution - How Many Customers is Employee 4 the Sales Support Agent For?
 */
 SELECT COUNT(*) FROM Customer WHERE SupportRepId = 4;
 
 /*
-Challenge 2 Solution - How Many Customers is Jane Peacock the Sales Support Agent For?
+Challenge 2 and Solution - How Many Customers is Jane Peacock the Sales Support Agent For?
 */
 SELECT Employee.FirstName AS Employee, COUNT(Customer.FirstName) AS Customer FROM Employee
 JOIN Customer ON Customer.SupportRepId = Employee.EmployeeId
 WHERE Employee.FirstName = 'Jane';
 
 /*
-Challenge 4 Solution - Which Media Type is most popular? How could you answer this with a single query?
+Challenge 3 and Solution - Which Media Type is most popular? How could you answer this with a single query?
 You probably cannot based on what you know so far. We will get there.
 */
