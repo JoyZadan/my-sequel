44087da91a35ffc3473060e151a5ff11552cff29
JoyZadan <joyzadan.learning@gmail.com>
2022-04-19 14:03:01 +0000

    Created queries_where.sql file, added queries script walkthroughs and challenge solution

---
+++ queries_where.sql
@@ -0,0 +1,19 @@
+/*
+Select all `Track` records where the `Composer` is `U2`
+*/
+select * from Track
+where Composer = 'U2';
+
+/*
+Select all `Album` records where the `AlbumId` is 232;
+*/
+select * from Album
+where AlbumId = 232;
+
+/*
+Challenge 1 Solution - Select the 'FirstName', 'LastName' and 'Title' Columns
+from the 'Employee' Table. Filtering the results to only those with a Title of
+'IT Staff'.
+*/
+select FirstName, LastName, Title from Employee
+where Title = "IT Staff";
