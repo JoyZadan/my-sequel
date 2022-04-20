fa7b2a45b9d5519030a1c40684618cc198ba0d2e
JoyZadan <joyzadan.learning@gmail.com>
2022-04-20 03:02:46 +0000

    Use where clause to identify to be deleted rows, and used Python to dynamically alter the number of %s placeholders based on the number of to be deleted items in the list

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -16,25 +16,14 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    # To return data to the application, we need to fetch the data
-    # By using a cursor method
-    # To retrieve the data into a variable which is iterable
-    # Use for each which returns the data where each row is a tuple
-    # Change type of cursor by passing pymysql.cursors.DictCursor
-    # As an argument when creating the cursor
-    # Using DictCursor returns rows that include the column names
-    # This format also lends itself to being converted into JSON
-    # Lines 30 to 33 must be indented if not commented out
-    # with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-    # SEQUEL = "SELECT * FROM Genre;"
-    # cursor.execute(SEQUEL)
-    # for row in cursor:
-    # print(row)
-    # Creates a table (replaced code from walkthrough with below)
-    # Replacing single row insert with multiple rows insert using
-    # executemany
     with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM Friends WHERE name in ('jim', 'bob')")
+        list_of_names = ['fred', 'Fred', 'jane', 'anne']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
